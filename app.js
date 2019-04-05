58743c1d82944cc9fa0d9b79d05dd81322cd4746
Charles_Sin <charlessin1993@gmail.com>
2019-04-05 16:06:56 +0800

    Adding restful api

--- app.js
+++ app.js
@@ -9,12 +9,27 @@ app.get('/', function (req,res){
     res.sendFile(path.join(__dirname + '/src/signin.html'));
 })
 
-app.get('/signin_style.css', function (req,res){
-    res.sendFile(path.join(__dirname + '/src/css/signin.css'));
+app.get('/src/css/style.css', function (req,res){
+    res.sendFile(path.join(__dirname + '/src/css/style.css'));
 })
 
 app.get('/signup_style.css', function (req,res){
     res.sendFile(path.join(__dirname + '/src/css/signup.css'));
+})
+
+app.get('/sign_in', function (req,res){
+    res.send("SING_IN");
+    console.log("USER_SIGN_IN");
+})
+
+app.get('/sign_up', function (req,res){
+    res.send("SING_UP");
+    console.log("USER_SIGN_UP");
+})
+
+app.get('/register_account', function (req,res){
+    res.send("REGISTER");
+    console.log("REGISTER_NEW_USER");
 })
 
 app.listen(3000, function(){
