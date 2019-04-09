a223ce2c11f39af06e054485d33c93cf003c0161
unknown <d0561910@fcu.edu.tw>
2019-04-09 11:41:49 +0800

    setting script.js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,8 +1,23 @@
 $('#reg_btn').click(function(){
-    location.replace("/register_account");
+    location.replace("/sign_up");
 })
 
 $('#log_btn').click(function(){
     location.replace("/");
 })
 
+$('#btn_signup').click(function() {
+    var username = $('#name_input').val();
+    var useremail = $('#email_input').val();
+    var pass1 = $('#pass_input').val();
+    var pass2 = $('#second_pass_input').val();
+
+    console.log("username" + username);
+    console.log("useremail" + useremail);
+    console.log("pass1" + pass1);
+    console.log("pass2" + pass2);
+
+    if(pass1 !== pass2){
+        alert("Password not match!!!");
+    }
+})
