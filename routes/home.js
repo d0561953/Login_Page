79b75e4a970748774012c362d71feb2724514467
CharlesSin <d0561910@fcu.edu.tw>
2020-04-28 17:09:47 +0800

    adding JWT

---
+++ routes/home.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* Get Login Page. */
+router.get('/', verifyToken, function (req, res, next) {
+    res.render('home');
+});
+
+// Verify Token
+function verifyToken(req, res, next) {
+    // Get auth header value
+    const bearerHeader = req.headers["authorization"];
+    // Check if bearer is undefined
+    if (typeof bearerHeader !== "undefined") {
+        // Split at the space
+        const bearer = bearerHeader.split(" ");
+        // Get token from array
+        const bearerToken = bearer[1];
+        // Set the token
+        req.token = bearerToken;
+        // Next middleware
+        next();
+    } else {
+        // Forbidden
+        res.sendStatus(403);
+    }
+}
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const user = {
+    id: 1,
+    username: "test",
+    email: "test@gmail.com"
+};
+
 /* Get Login Page. */
 router.get('/', function (req, res, next) {
     res.render('signin');
+});
+
+router.post('/login', function (req, res, next) {
+    jwt.sign({ user }, "secretkey", { expiresIn: "60m" }, (err, token) => {
+        req.headers["authorization"] = `bearer ${token}`;
+        res.redirect('/home');
+        // res.json({
+        //     token
+        // });
+    });
 });
 
 module.exports = router;
